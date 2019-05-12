4c363bb2ba74d7f540f42c717076195ed26ce200
taraxacum <john980118@163.com>
2019-05-12 20:20:21 +0800

    client comment

--- ipc_lab/src/client.c
+++ ipc_lab/src/client.c
@@ -19,25 +19,22 @@ void Fputs(const char* ptr, FILE* stream)
 
 int main(int argc, char** argv)
 {
-    int clientfd;
-    char *host, *port, buf[MAXLINE];
-    rio_t rio;
-
     if (argc != 3) {
         fprintf(stderr, "usage: %s <host> <port>\n", argv[0]);
         exit(0);
     }
-    host = argv[1];
-    port = argv[2];
 
-    clientfd = Open_clientfd(host, port);
+    rio_t rio;
+    int clientfd = Open_clientfd(argv[1], argv[2]);
     Rio_readinitb(&rio, clientfd);
 
+    char buf[MAXLINE];
     while (Fgets(buf, MAXLINE, stdin) != NULL) {
         Rio_writen(clientfd, buf, strlen(buf));
         Rio_readlineb(&rio, buf, MAXLINE);
         Fputs(buf, stdout);
     }
+    
     Close(clientfd);
     exit(0);
 }
